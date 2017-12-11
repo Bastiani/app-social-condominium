20887475b00c7a59e81829267dd892d902204398
Rafael Bastiani <rafacdb@gmail.com>
2017-12-11 11:12:37 -0200

    solution for query Person

--- src/Queries/PersonsQuery.js
+++ src/Queries/PersonsQuery.js
@@ -5,33 +5,31 @@ import { QueryRenderer, graphql } from 'react-relay';
 import environment from '../createRelayEnvironment';
 import PersonInfo from '../Queries/PersonInfo';
 
-export default class PersonsQuery extends React.Component {
-  render() {
-    return (
-      <View style={{ paddingVertical: 20 }}>
-        <QueryRenderer
-          environment={environment}
-          query={graphql`
-            query PersonsQuery($name: String!) {
-              persons(name: $name) {
-                ...PersonInfo_person
-              }
-            }
-          `}
-          variables={{
-            name: 'R',
-          }}
-          render={({ error, props }) => {
-            if (error) {
-              return <Text>{error.message}</Text>;
-            } else if (props) {
-              console.log(`Query====${props.persons}`);
-              return <PersonInfo person={props.persons} />;
-            }
-            return <Text>Loading</Text>;
-          }}
-        />
-      </View>
-    );
-  }
-}
+const PersonsQuery = () => (
+  <View style={{ paddingVertical: 20 }}>
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+        query PersonsQuery($name: String!) {
+          persons(name: $name) {
+            ...PersonInfo_persons
+          }
+        }
+      `}
+      variables={{
+        name: 'R',
+      }}
+      render={({ error, props }) => {
+        if (error) {
+          return <Text>{error.message}</Text>;
+        } else if (props) {
+          console.log(`Query====${props.persons}`);
+          return <PersonInfo persons={props.persons} />;
+        }
+        return <Text>Loading</Text>;
+      }}
+    />
+  </View>
+);
+
+export default PersonsQuery;
